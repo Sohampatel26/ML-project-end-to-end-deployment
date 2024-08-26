fd7ee88a82b8d484ca7afa01a8fe9bddf837ce5a
Soham Patel <72576227+Sohampatel26@users.noreply.github.com>
2024-08-26 15:21:57 -0700

    Model trainer and transformer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,7 +4,7 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from src.utils import save_object
-
+from src.components.model_trainer import ModelTrainer
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
@@ -54,5 +54,7 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    # modeltrainer=ModelTrainer()
-    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
